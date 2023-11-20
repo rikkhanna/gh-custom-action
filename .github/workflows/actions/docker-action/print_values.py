78ea831e054a5796e2710b2fc3947ec32a4d2b11
rikkhanna <rishabhkhanna726@gmail.com>
2023-11-21 00:23:42 +0530

    Testing docker action

---
+++ .github/workflows/actions/docker-action/print_values.py
@@ -0,0 +1,6 @@
+print("Hello, Docker!")
+print("Printing some values:")
+print("Value 1: 42")
+print("Value 2: Docker is awesome!")
+print("Package name - ", package-name)
+action-out="Docker Action output"
