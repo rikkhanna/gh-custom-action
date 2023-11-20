3ec6bdec59f68081eb56f6073cb29f64e3da3ba7
rikkhanna <rishabhkhanna726@gmail.com>
2023-11-21 00:33:15 +0530

    Updated docker action

--- .github/workflows/actions/docker-action/print_values.py
+++
@@ -1,6 +0,0 @@
-print("Hello, Docker!")
-print("Printing some values:")
-print("Value 1: 42")
-print("Value 2: Docker is awesome!")
-print("Package name - ", package-name)
-action-out="Docker Action output"
---
+++ .github/workflows/actions/docker-action/print_values.sh
@@ -0,0 +1,2 @@
+echo "Package Name -  $package_name"
+action_out="Docker Action output" >> $GITHUB_OUTPUT
